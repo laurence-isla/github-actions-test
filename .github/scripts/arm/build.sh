fc5b9fb6aecadebec618374b54f670215e075015
Laurence Isla <lau.isla.c@gmail.com>
2022-04-23 01:00:03 -0500

    Fix dockerfile and remove armv7 builds

--- .github/scripts/arm/build.sh
+++ .github/scripts/arm/build.sh
@@ -21,6 +21,7 @@ install_ghcup() {
   export BOOTSTRAP_HASKELL_NONINTERACTIVE=1
   export BOOTSTRAP_HASKELL_MINIMAL=1
   curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+  source ~/.ghcup/env
 }
 
 install_cabal() {
@@ -36,11 +37,10 @@ install_ghc() {
 
 install_packages
 
-ghcup --version || install_ghcup
-
 # Add ghcup to the PATH for this session
 [ -f ~/.ghcup/env ] && source ~/.ghcup/env
 
+ghcup --version || install_ghcup
 cabal --version || install_cabal
 ghc --version || install_ghc
 
